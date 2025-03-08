f4959ae85210fe146820a79775e64ff397199a10
kangseong <kim.kangseong@gmail.com>
2025-03-08 17:45:10 +0900

    ~44강

--- Assets/Scripts/ActionButtonUI.cs
+++ Assets/Scripts/ActionButtonUI.cs
@@ -8,13 +8,23 @@ public class ActionButtonUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI TextMeshPro;
     [SerializeField] private Button button;
+    [SerializeField] private GameObject selectedGameObject;
+
+    private BaseAction baseAction;
 
     public void SetBaseAction(BaseAction baseAction)
     {
+        this.baseAction = baseAction;
         TextMeshPro.text = baseAction.GetActionName().ToUpper();
 
         button.onClick.AddListener(() => {
             UnitActionSystem.Instance.SetSelectedAction(baseAction);
         });
+    }
+
+    public void UpdateSelectedVisual()
+    {
+        BaseAction selectedBaseAction = UnitActionSystem.Instance.GetSelectedAction();
+        selectedGameObject.SetActive(selectedBaseAction == baseAction);
     }
 }
