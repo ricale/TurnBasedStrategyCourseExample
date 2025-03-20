0ce198dfb4a49fad6d5eea51bfa4a895ace682e9
kangseong <kim.kangseong@gmail.com>
2025-03-20 22:00:43 +0900

    ~65강

--- Assets/Scripts/PathNode.cs
+++ Assets/Scripts/PathNode.cs
@@ -34,4 +34,39 @@ public class PathNode
     {
         return fCost;
     }
+
+    public void SetGCost(int gCost)
+    {
+        this.gCost = gCost;
+    }
+
+    public void SetHCost(int fCost)
+    {
+        this.fCost = fCost;
+    }
+
+    public void CalculateFCost ()
+    {
+        fCost = gCost + hCost;
+    }
+
+    public void ResetCameFromPathNode()
+    {
+        cameFromPathNode = null;
+    }
+
+    public void SetCameFromPathNode(PathNode pathNode)
+    {
+        cameFromPathNode = pathNode;
+    }
+
+    public PathNode GetCameFromPathNode()
+    {
+        return cameFromPathNode;
+    }
+
+    public GridPosition GetGridPosition()
+    {
+        return gridPosition;
+    }
 }
