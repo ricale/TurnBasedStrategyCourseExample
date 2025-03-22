ebeba9c1840b27f82641dee2a0d6cc21a5a688c6
kangseong <kim.kangseong@gmail.com>
2025-03-22 18:20:29 +0900

    ~70강

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ShootAction : BaseAction
 {
+    public static event EventHandler<OnShootEventArgs> OnAnyShoot;
     public event EventHandler<OnShootEventArgs> OnShoot;
 
     public class OnShootEventArgs : EventArgs
@@ -88,6 +89,10 @@ public class ShootAction : BaseAction
 
     private void Shoot()
     {
+        OnAnyShoot?.Invoke(this, new OnShootEventArgs {
+            targetUnit = targetUnit,
+            shootingUnit = unit,
+        });
         OnShoot?.Invoke(this, new OnShootEventArgs {
             targetUnit = targetUnit,
             shootingUnit = unit,
